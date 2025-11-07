31443b67dba61b140c1730231d325704cce644b8
DevGGamer <javageorgygeras@gmail.com>
2025-11-07 23:44:48 +0300

    Header, Main Section new design

--- frontend/src/Pages/StartPage/StartPageSectors/MainSection/MainSection.tsx
+++ frontend/src/Pages/StartPage/StartPageSectors/MainSection/MainSection.tsx
@@ -1,29 +1,15 @@
 import React from "react";
 import styles from './MainSection.module.css';
-import TrialLessonButton from "../../../../Components/TrialLessonButton/TrialLessonButton";
-import VRCharacterImage from '../../../../Images/StartPage/vrcharacter.png'
 
 const MainSection: React.FC = () => {
     return (
         <section id={styles.main_section}>
-            <div className={styles.content_wrapper}>
-                <div className={styles.left_block}>
-                    <h1>
-                        <span>ИЗУЧИ КОД,</span><br />
-                        <span>КАК ЕСЛИ БЫ</span><br />
-                        <span>ТЫ БЫЛ В ИГРЕ!</span>
-                    </h1>
-                    <p>Стань создателем цифровых миров, играя и обучаясь у нас</p>
-                    <TrialLessonButton className={styles.trial_lesson_button}/>
-
-                    <div className={styles.right_block}>
-                        <img
-                            src={VRCharacterImage}
-                            alt="Мальчик в VR"
-                            className={styles.hero_image}
-                        />
-                    </div>
+            <div className={styles.central_card_container}>
+                <div className={styles.central_card}>
+                    <h1>Создавай свои миры с PixelVerse</h1>
+                    <p>Обучение программированию для детей и подростков</p>
                 </div>
+                <div id={styles.button} className="button button-with-accent-color">Начать обучение</div>
             </div>
         </section>
     );
